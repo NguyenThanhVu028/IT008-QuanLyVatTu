1fadb3abb8a6f44dc7080c034f15dfed289d96a9
Vũ Nguyễn Thanh <23521811@ms.uit.edu.vn>
2024-10-05 16:02:30 +0700

    Upload An App

---
+++ WinFormTest/Program.cs
@@ -0,0 +1,12 @@
+namespace WinFormTest
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MyForm());
+        }
+    }
+}
