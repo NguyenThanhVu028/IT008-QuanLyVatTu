90da1957e2330757a5af14f5b6e6d304ebd9a247
Vũ Nguyễn Thanh <23521811@ms.uit.edu.vn>
2024-10-25 11:43:18 +0700

    Hello

--- WinFormTest/obj/Debug/net8.0-windows/WinFormTest.AssemblyInfo.cs
+++ WinFormTest/obj/Debug/net8.0-windows/WinFormTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403b6d83e64e9e068ecf2b32cfce29920805a25a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8185c47c29fea9ec9689db284b2755261a46ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
