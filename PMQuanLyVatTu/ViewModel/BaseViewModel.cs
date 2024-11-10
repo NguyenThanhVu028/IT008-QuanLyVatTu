c52c475b15fd0d21c9ef541b22af8ddf22b46e9c
Vũ Nguyễn Thanh <23521811@ms.uit.edu.vn>
2024-11-10 12:11:07 +0700

    Create Login Window

---
+++ PMQuanLyVatTu/ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace PMQuanLyVatTu.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
